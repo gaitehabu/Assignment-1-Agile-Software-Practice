3816aab53924e6cb7545aec2e4ccdaa511166223
Yan Hu <1026437307@qq.com>
2019-11-03 13:21:21 +0000

    Test GET /users/:id AND /users/name/:name

--- test/user-test.js
+++ test/user-test.js
@@ -69,5 +69,61 @@ describe('Users: models', function () {
                 });
         });
     });
+
+    describe('GET /users/:id', () => {
+        describe('when the id is valid', () => {
+            it('should return the matching user', done => {
+                request(server)
+                    .get('/users/5db596749ab793039c1a5a6f')
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        expect(res.body).to.have.property("name", "Francis");
+                        expect(res.body).to.have.property("password", "123456789");
+                        done(err);
+                    });
+            });
+        });
+        describe('when the id is invalid', () => {
+            it('should return the NOT Found message', done => {
+                request(server)
+                    .get('/users/1119999')
+                    .expect(200)
+                    .end((err, res) => {
+                        expect({message: "User NOT Found By ID!!"});
+                        done(err);
+                    })
+            });
+        });
+    });
+
+    describe('GET /users/name/:name', () => {
+        describe('when the name is valid', () => {
+            it('should return the matching user', done => {
+                request(server)
+                    .get('/users/name/Francis')
+                    .set("Accept", "application/json")
+                    .expect("Content-Type", /json/)
+                    .expect(200)
+                    .end((err, res) => {
+                        expect(res.body).to.have.property("name", "Francis");
+                        expect(res.body).to.have.property("password", "123456789");
+                        done(err);
+                    });
+            });
+        });
+        describe('when the name is invalid', () => {
+            it('should return the NOT Found message', done => {
+                request(server)
+                    .get('/users/name/9999')
+                    .expect(200)
+                    .end((err, res) => {
+                        expect({message: "User NOT Found By Name!!"});
+                        done(err);
+                    })
+            });
+        });
+    });
     
 });
